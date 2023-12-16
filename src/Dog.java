d8208688fb2fe38e4ce6374099ad58305f36c231
g21turbo <dg21stew@gmail.com>
2023-12-16 11:59:13 -0800

    overridden method added

--- src/Dog.java
+++ src/Dog.java
@@ -23,4 +23,14 @@ public class Dog extends Animal{
                 ", tailShape='" + tailShape + '\'' +
                 "} " + super.toString();
     }
+
+    public void makeNoise() {
+
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+        System.out.println("Dogs walk, run and wag their tail");
+    }
 }
